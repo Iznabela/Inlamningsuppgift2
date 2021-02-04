140decdb3571a7368d4324a876fa962fea003cd8
Isabella <isabellapetterson@gmail.com>
2021-02-04 19:55:44 +0100

    checkboxes click

--- main.js
+++ main.js
@@ -2,5 +2,37 @@
 
 
 function showInfo() {
-    document.getElementById('weather').style.visibility = 'visible';
+    let weatherCheckbox = document.getElementById('checkbox-weather');
+    let attractionsCheckbox = document.getElementById('checkbox-attractions');
+    let filter = document.getElementById('filter');
+    let searchButton = document.getElementById('search-button');
+
+    if (weatherCheckbox.checked == true) {
+        document.getElementById('weather').style.visibility = 'visible';
+    }
+    else {
+        document.getElementById('weather').style.visibility = 'hidden';
+    }
+    
+    if (attractionsCheckbox.checked == true) {
+        document.getElementById('attractions-title').style.visibility = 'visible';
+        const attractionBoxes = document.querySelectorAll('.attraction-box');
+    
+        attractionBoxes.forEach(element => {
+        element.style.visibility = 'visible';
+        })
+    }
+    else {
+        document.getElementById('attractions-title').style.visibility = 'hidden';
+        const attractionBoxes = document.querySelectorAll('.attraction-box');
+    
+        attractionBoxes.forEach(element => {
+        element.style.visibility = 'hidden';
+        })
+    }
+    
+
+    
+    
+
 }
